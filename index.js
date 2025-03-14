368b966a19d9bb4d4115ea62975a4e7603430309
Joseph <j.mburu.pro@gmail.com>
2025-03-14 14:05:41 +0300

    changed html

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+let p = document.querySelector("p#text")
+p.innerHTML = 'This is so cool!'
+console.log(p.innerHTML = 'This is really cool!')
+})
